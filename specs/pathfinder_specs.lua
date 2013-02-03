46f454927e2ed6443d6fcc287c3ed515110188e0
Roland Yonaba <roland.yonaba@gmail.com>
2013-02-03 21:30:07 +0000

    Pushed grid specs

---
+++ specs/pathfinder_specs.lua
@@ -0,0 +1,12 @@
+context('Module Pathfinder', function()
+	local PF
+	
+	before(function()
+		PF = require ('jumper.pathfinder')
+  end)
+	
+  context('The Pathfinder Class', function()
+		
+	end)
+
+end)
