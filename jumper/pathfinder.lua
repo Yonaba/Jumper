a92bf851b18753466dcc98221d1b3594569f9aab
Roland Yonaba <roland.yonaba@gmail.com>
2013-02-06 14:25:50 +0000

    Untag version and release date

--- jumper/pathfinder.lua
+++ jumper/pathfinder.lua
@@ -7,8 +7,8 @@
 -- @license <a href="http://www.opensource.org/licenses/mit-license.php">MIT</a>
 -- @module jumper.pathfinder
 
-local _VERSION = "1.8.0"
-local _RELEASEDATE = "01/26/2013"
+local _VERSION = ""
+local _RELEASEDATE = ""
 
 --- @usage
 local usage = [[
