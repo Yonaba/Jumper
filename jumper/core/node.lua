6003026e34f87e0ace56523381ce4e78f2ae693d
Roland Yonaba <roland.yonaba@gmail.com>
2013-03-19 22:11:44 +0000

    Removed License footer Updated docs reference

--- jumper/core/node.lua
+++ jumper/core/node.lua
@@ -1,36 +1,31 @@
---- <strong>The <code>node</code> class</strong>.
--- The `node` class represents a cell on a collision map. Basically, for each single cell
--- in the collision map passed-in upon initialization, a `node` object would be generated
--- and then stored within the `grid` object.
+--- The Node class.
+-- The `node` represents a cell (or a tile) on a collision map. Basically, for each single cell (tile)
+-- in the collision map passed-in upon initialization, a `node` object will be generated
+-- and then cached within the `grid`.
 --
 -- In the following implementation, nodes can be compared using the `<` operator. The comparison is
--- made on the basis of their `f` cost. From a processed node, the `pathfinder` would expand the search 
--- to the next neighbouring node having the lowest `f` cost. This comparison is internally used within the
--- *open list* `heap` to quickly sort all nodes queued inside the heap.
+-- made with regards of their `f` cost. From a given node being examined, the `pathfinder` will expand the search 
+-- to the next neighbouring node having the lowest `f` cost. See `bheap` for more details.
 -- 
--- @author Roland Yonaba
--- @copyright 2012-2013
--- @license <a href="http://www.opensource.org/licenses/mit-license.php">MIT</a>
--- @module jumper.core.node
+-- @module node
 
 if (...) then
 
 	local assert = assert
 	
-  --- Internal `node` Class
-  -- @class table
-  -- @name node
-  -- @field x the x-coordinate of the node on the collision map
-  -- @field y the y-coordinate of the node on the collision map
+	--- The `Node` class.<br/>
+	-- This class is callable.
+	-- Therefore,_ <code>Node(...)</code> _acts as a shortcut to_ <code>Node:new(...)</code>.
+	-- @type Node
   local Node = {}
   Node.__index = Node
 
-  --- Inits a new `node` object
+  --- Inits a new `node`
   -- @class function
-  -- @name node:new
   -- @tparam int x the x-coordinate of the node on the collision map
   -- @tparam int y the y-coordinate of the node on the collision map
-  -- @treturn node a new `node` object
+  -- @treturn node a new `node`
+	-- @usage local node = Node(3,4)
   function Node:new(x,y)
     return setmetatable({_x = x, _y = y}, Node)
   end
@@ -39,23 +34,25 @@ if (...) then
   -- Will be used to sort a collection of nodes in a binary heap on the basis of their F-cost
   function Node.__lt(A,B) return (A._f < B._f) end
 
-  --- Returns `x`-coordinate of the `node`
+  --- Returns x-coordinate of a `node`
   -- @class function
-  -- @name node:getX
-  -- @treturn number the x-coordinate of the `node`	
+  -- @treturn number the x-coordinate of the `node`
+	-- @usage local x = node:getX()	
 	function Node:getX() return self._x end
 	
-  --- Returns `y`-coordinate of the `node`
+  --- Returns y-coordinate of a `node`
   -- @class function
   -- @name node:getY
   -- @treturn number the y-coordinate of the `node`	
+	-- @usage local y = node:getY()		
 	function Node:getY() return self._y end
 	
-  --- Returns `x` and `y` coordinates of the `node`
+  --- Returns x and y coordinates of a `node`
   -- @class function
   -- @name node:getPos
   -- @treturn number the x-coordinate of the `node`
   -- @treturn number the y-coordinate of the `node`
+	-- @usage local x, y = node:getPos()		
 	function Node:getPos() return self._x, self._y end
 	
   return setmetatable(Node,
@@ -64,27 +61,3 @@ if (...) then
 		end}
 	)
 end
-
-
---[[
-Copyright (c) 2012-2013 Roland Yonaba
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---]]
